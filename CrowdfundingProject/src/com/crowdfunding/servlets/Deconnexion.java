91e96003df80d2cfecd547355ea0387843dfc0c1
Deuneuv <makdeuneuv@gmail.com>
2016-03-25 14:25:45 +0100

    Création de la servlet 'Participation.java' pour la contribution à un projet. Reste plus qu'à tester avec les champs de la jsp correspondante "projet.jsp"

--- CrowdfundingProject/src/com/crowdfunding/servlets/Deconnexion.java
+++ CrowdfundingProject/src/com/crowdfunding/servlets/Deconnexion.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 public class Deconnexion extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	public static final String VUE = "/WEB-INF/connexion";
+	public static final String VUE = "/WEB-INF/accueil.jsp";
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /* Récupération et destruction de la session en cours */
--- CrowdfundingProject/src/com/crowdfunding/servlets/Participation.java
+++ CrowdfundingProject/src/com/crowdfunding/servlets/Participation.java
@@ -8,45 +8,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.crowdfunding.beans.Projet;
-import com.crowdfunding.beans.ProjetValidation;
 import com.crowdfunding.beans.Utilisateur;
 import com.crowdfunding.dao.DAOFactory;
 import com.crowdfunding.dao.ProjetDao;
 
-@WebServlet("/ProjetServlet")
-public class ProjetServlet extends HttpServlet {
+@WebServlet("/Participation")
+public class Participation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public static final String CONF_DAO_FACTORY = "daofactory";
-	public static final String ATT_FORM         = "projetValidation";
-	public static final String ATT_PROJET         = "projet";
-	public static final String VUE1             = "/WEB-INF/createProjet.jsp";
-	public static final String VUE2             = "/WEB-INF/projet.jsp";
-	
+	public static final String VUE1             = "/WEB-INF/projet.jsp";
 	private ProjetDao     projetDao;
-    
+
     public void init() throws ServletException{
     	this.projetDao = ( (DAOFactory) getServletContext().getAttribute( CONF_DAO_FACTORY ) ).getProjetDao();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher(VUE1).forward(request, response);
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ProjetValidation form = new ProjetValidation(projetDao );
-		
 		HttpSession session = request.getSession();
 		Utilisateur u = (Utilisateur) session.getAttribute("sessionUtilisateur");
-
-        /* Traitement de la requête et récupération du bean en résultant */
-		Projet projet = form.inscrireProjet( request, u.getId());
-
-        /* Stockage du formulaire et du bean dans l'objet request */
-        request.setAttribute( ATT_FORM, form );
-        request.setAttribute(ATT_PROJET, projet);
-        this.getServletContext().getRequestDispatcher(VUE2).forward(request, response);
+		projetDao.participation(u.getId(),(int) request.getAttribute("idUser"),(int) request.getAttribute("montant"));
 	}
 
 }
